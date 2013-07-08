08ad2b33125b33685ce95885bcb2f55a437235c3
AeroNotix <aaron.l.france@gmail.com>
2013-07-08 10:40:23 +0100

    Added clabango to the dependencies.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A small blogging engine"
   :license "BSD"
   :url "http://github.com/AeroNotix/clowl"
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clabango "0.5"]])
