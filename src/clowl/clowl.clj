93458ba31d952fe70922698504cf764e38038804
AeroNotix <aaron.l.france@gmail.com>
2013-07-08 12:03:51 +0100

    Now we extract the blog_post from the database.

--- src/clowl/clowl.clj
+++ src/clowl/clowl.clj
@@ -1,10 +1,27 @@
 (ns clowl.clowl
   (:use compojure.core)
+  (:use clowl.connection)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.java.jdbc :as j]
+            [clojure.java.jdbc.sql :as s]))
+
+(defn get-blog [blogname]
+  (j/query mysql-db
+           (s/select :blog_post :blog_entry (s/where {:blog_url blogname}))))
+
+(defn blog [blogname]
+  (do
+    (println (type (seq (get-blog blogname))))
+    (seq (get-blog blogname))))
 
 (defroutes app-routes
-  (GET "/" [] "<h1>Hello World</h1>")
+  (GET "/blog/:blogname" [blogname]
+       (blog blogname))
+  (GET "/" []
+       (blog "base"))
+  (GET "/about" []
+       (blog "about"))
   (route/not-found "<h1>Page not found</h1>"))
 
 (def app
