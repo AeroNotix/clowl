85fe9822f49159eac2f9ad24a887cb8aab0c3d1f
AeroNotix <aaron.l.france@gmail.com>
2013-07-08 11:26:47 +0100

    Added dependencies to mysql and the clojure jdbc driver.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :url "https://github.com/AeroNotix/clowl"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [clabango "0.5"]]
+                 [clabango "0.5"]
+                 [org.clojure/java.jdbc "0.3.0-alpha4"]
+                 [mysql/mysql-connector-java "5.1.25"]]
   :ring {:handler clowl.clowl/app}
   :plugins [[lein-ring "0.8.5"]])
